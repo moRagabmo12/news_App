0775db10eaef4be0091d95f3da83f7328cda5fc9
moRagabmo12 <moragabmohamed12@gmail.com>
2024-07-24 20:43:04 +0300

    cached network image

--- lib/widgets/newsTile.dart
+++ lib/widgets/newsTile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/articleModel.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class newsTile extends StatelessWidget {
   const newsTile({super.key, required this.article});
@@ -12,12 +13,11 @@ class newsTile extends StatelessWidget {
       children: [
         ClipRRect(
           borderRadius: BorderRadius.circular(12),
-          child: Image.network(
-            article.image ??
+          child: CachedNetworkImage(
+            imageUrl: article.image ??
                 'https://images.skynewsarabia.com/images/v1/2024/05/01/1710454/800/450/1-1710454.jpg',
-            height: 200,
-            width: 400,
-            fit: BoxFit.cover,
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
           ),
         ),
         Text(
